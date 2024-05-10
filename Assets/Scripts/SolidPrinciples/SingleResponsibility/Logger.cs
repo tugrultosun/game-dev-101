ed5188525417457ae4c97a5dc8cdac91b4612c0e
tuğrul tosun <ttosun350@gmail.com>
2024-05-10 14:42:11 +0300

    adds solid principles respectively single responsibility principle

---
+++ Assets/Scripts/SolidPrinciples/SingleResponsibility/Logger.cs
@@ -0,0 +1,12 @@
+public class Logger
+{
+    public void Log(Player player)
+    {
+        // todo : print player related data xp,kills,deaths,playTime
+    }
+
+    public void Log(Monster monster)
+    {
+        // todo : print monster related data hp,crit chance etc
+    }
+}
